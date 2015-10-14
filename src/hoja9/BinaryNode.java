c0cf5f710c96794f91078dacb2b6e5af20361a90
sebascas <frnndsebatian@hotmail.com>
2015-10-14 14:47:53 -0600

    BinaryNode

---
+++ src/hoja9/BinaryNode.java
@@ -0,0 +1,33 @@
+package hoja9;
+
+/*-----------------------------------------------------------------------
+Carlos Javier Lima Cordon			14373
+Fernando Sebastian Castillo Echeverria		14102
+Marlon Josue Castillo Martinez                  14427
+
+Algoritmos y estructura de datos
+Seccion 30
+Hoja de trabajo 9
+
+  * Codigo obtenido de:
+  *  Implements a top-down splay tree.
+ * Available at http://www.link.cs.cmu.edu/splay/
+ * Author: Danny Sleator <sleator@cs.cmu.edu>
+ * This code is in the public domain.
+ 
+-------------------------------------------------------------------------*/ 
+  
+
+
+class BinaryNode
+{
+    BinaryNode(Comparable theKey) {
+        key = theKey;
+        left = right = null;
+    }   
+
+    Comparable key;          // The data in the node
+    BinaryNode left;         // Left child
+    BinaryNode right;        // Right child
+    
+}
