7a4f17989e2347a7b33824faa6d5d9a3fb4863f1
sebascas <frnndsebatian@hotmail.com>
2015-10-14 14:48:36 -0600

    SimpleSet

---
+++ src/hoja9/SimpleSet.java
@@ -0,0 +1,39 @@
+package hoja9;
+
+ /*-----------------------------------------------------------------------
+Carlos Javier Lima Cordon			14373
+Fernando Sebastian Castillo Echeverria		14102
+Marlon Josue Castillo Martinez                  14427
+
+Algoritmos y estructura de datos
+Seccion 30
+Hoja de trabajo 9
+
+* Descripción: SimpleSet ejemplo con ArrayList.
+ 
+-------------------------------------------------------------------------*/ 
+    
+
+import java.util.ArrayList;
+
+class SimpleSet implements WordSet
+{
+	private ArrayList<Word> base;
+	
+	public SimpleSet()
+	{
+		base = new ArrayList<Word>();
+	}
+	
+	public Word get(Word word)
+	{
+		int index = base.indexOf(word);
+		if(index == -1) return null;
+		return base.get(index);
+	}
+	
+	public void add(Word wordObject)
+	{
+		base.add(wordObject);
+	}
+}
